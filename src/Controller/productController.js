a7a6a3dfefe4a08d3d1e38c552dffa4d4da47beb
preethuc <preetha@sparkouttech.com>
2022-12-02 09:52:07 +0530

    dynamically file upload edit

--- src/Controller/productController.js
+++ src/Controller/productController.js
@@ -1,21 +1,21 @@
 import Product from "./../Model/productModel";
 
-// File Path to store the CollectionImage
+//dynamic-image upload
 exports.createDynamicImage = async (req, res) => {
   try {
     const newImage = await new Product(req.body);
-console.log(req.file);
-        if (req.file) {
-          console.log(req.file);
-          newImage.product_image = req.file.path;
-        }
-        newImage.save();
-    
-        res.status(201).json({
-          status: "success",
-          message: "successfully created",
-          image: newImage.product_image,
-        });
+    console.log(req.file);
+    if (req.file) {
+      console.log(req.file);
+      newImage.product_image = req.file.path;
+    }
+    newImage.save();
+
+    res.status(201).json({
+      status: "success",
+      message: "successfully created",
+      image: newImage.product_image,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -24,22 +24,11 @@ console.log(req.file);
 //POST - create product
 exports.createProduct = async (req, res) => {
   try {
-    // const filename = Date.now() + "__" + req.files.product_image.name;
-    // const file = req.files.product_image;
-
-    // console.log(file);
-
-    // var dir = "./temp/" + filename ;
-    // if (!fs.existsSync(dir)) {
-    //   fs.mkdirSync(filename);
-    // }
-    // file.mv(dir);
-
     const productData = await Product.create(req.body);
     return res.status(201).json({
       status: "success",
       message: "Product Created",
-      Data: productData
+      Data: productData,
     });
   } catch (error) {
     console.log(error.message);
--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ import categoryRoute from "./Route/categoryRoute";
 
 const app = express();
 app.use(morgan("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
