15c8d834490656c2fb713d1ef9e95936ddad8513
preethuc <preetha@sparkouttech.com>
2022-12-06 11:48:36 +0530

    changes in aggregation

--- src/Controller/categoryController.js
+++ src/Controller/categoryController.js
@@ -67,13 +67,18 @@ exports.CategoryListName = async (req, res) => {
       {
         $group: {
           _id: "$category",
-      
-          // _id: { $toUpper: "$category" },
-
-          // numProducts:{$sum:1},
+          numProducts: { $sum: 1 },
+          minProducts: { $min: "$product" },
+          // maxProducts:{$max:"$category"},
         },
       },
+
+      // {
+      //   // remove particular category
+      //   $match: { _id: { $ne: "FASHION" } },
+      // },
     ]);
+
     res.status(200).json({
       status: "success",
       message: "Category names List",
@@ -88,3 +93,48 @@ exports.CategoryListName = async (req, res) => {
   }
 };
 
+
+//GET - category by Id
+exports.getCategoryById = async (req, res) => {
+  try {
+    const data = await Category.findById(req.params.id)
+      .populate("sub_category", "sub_categeory_type")
+      .populate("product", "product_name");
+
+    return res.status(200).json({
+      status: "success",
+      result: data.length,
+      message: "Category List",
+      Data: data,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      Message: "ERROR Occured",
+      Error: error,
+    });
+  }
+};
+
+// exports.categoryUnderSub = async (req, res) => {
+//   try {
+//     const data = await Category.find({category:"GYM"})
+//       .populate("sub_category", "sub_categeory_type")
+//       // .populate("product", "product_name");
+
+//     return res.status(200).json({
+//       status: "success",
+//       result: data.length,
+//       message: "Category Under Sub-category",
+//       Data: {
+//         sub_categories: [data.sub_category] 
+//       }
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       status: "fail",
+//       Message: "ERROR Occured",
+//       Error: error,
+//     });
+//   }
+// }
--- src/Route/categoryRoute.js
+++ src/Route/categoryRoute.js
@@ -7,5 +7,9 @@ router.route("/createCat").post(categoryController.createCategory);
 router.route("/getCat").get(categoryController.getCategory);
 router.route("/fil/:cat").get(categoryController.filterCategory);
 router.route("/List").get(categoryController.CategoryListName);
+router.route("/catId/:id").get(categoryController.getCategoryById);
+// router.route("/order").get(categoryController.categoryUnderSub);
+
+
 
 module.exports = router;
